2f0cbb88e1a31ac69f540851658bfd41a91e9f34
Aditya Aneja <adityaaneja@localhost.localdomain>
2022-01-27 13:00:34 -0600

    Added more comments

--- app.js
+++ app.js
@@ -11,26 +11,26 @@ var handler = function(request, response) {
   if (requestCount > 5) {
     response.writeHead(500);
     response.end("I'm not well. Please restart me!");
-    requestCount=0; // reset the counter -aa 
+    requestCount=0; // Every sixth request returns 500. Reset the counter -aa 
     return;
   }
-  else if (request.url == '/forbidden') {
+  else if (request.url == '/forbidden') {  // request to the /forbidden endpoint returns 403
     response.writeHead(403);
     response.end('Forbidden Resource')
     return;
   }
-  else if (request.url == '/auth_req') {
+  else if (request.url == '/auth_req') {  //request to the /auth_req endpoint returns 401
     response.writeHead(401);
     response.end('Authorization Required');
     return;
   }
-  else if (request.url == '/bad_request') {
+  else if (request.url == '/bad_request') { //request to the /bad_request endpoint returns 400
     response.writeHead(400);
     response.end('Bad Request');
     return;
   }
   else {
-  response.writeHead(200);
+  response.writeHead(200);                 //Everything else returns 200
   response.end("You've hit " + os.hostname() + "\n");
   }
 };
